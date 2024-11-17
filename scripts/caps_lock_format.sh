1bce117dc480a2db28fc552fcbc854ff26c9f47f
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2024-11-17 18:22:14 +0100

    add attribute options

--- scripts/caps_lock_format.sh
+++ scripts/caps_lock_format.sh
@@ -13,31 +13,35 @@ default_caps_lock_on_bg=
 default_caps_lock_off_fg=
 default_caps_lock_off_bg=
 
-default_caps_lock_on_prefix=
-default_caps_lock_on_suffix=
-default_caps_lock_off_prefix=
-default_caps_lock_off_suffix=
+default_caps_lock_on_attr=
+default_caps_lock_off_attr=
 
 function main() {
-    local caps_lock_on_value="$(tmux-option "@caps_lock_on_value" "$default_caps_lock_on_value")"
-    local caps_lock_off_value="$(tmux-option "@caps_lock_off_value" "$default_caps_lock_off_value")"
+    local on_value="$(tmux-option "@caps_lock_on_value" "$default_caps_lock_on_value")"
+    local off_value="$(tmux-option "@caps_lock_off_value" "$default_caps_lock_off_value")"
 
-    local caps_lock_on_fg="$(tmux-option "@caps_lock_on_fg" "$default_caps_lock_on_fg")"
-    local caps_lock_on_bg="$(tmux-option "@caps_lock_on_bg" "$default_caps_lock_on_bg")"
-    local caps_lock_off_fg="$(tmux-option "@caps_lock_off_fg" "$default_caps_lock_off_fg")"
-    local caps_lock_off_bg="$(tmux-option "@caps_lock_off_bg" "$default_caps_lock_off_bg")"
+    local on_fg_color="$(tmux-option "@caps_lock_on_fg" "$default_caps_lock_on_fg")"
+    local on_bg_color="$(tmux-option "@caps_lock_on_bg" "$default_caps_lock_on_bg")"
+    local off_fg_color="$(tmux-option "@caps_lock_off_fg" "$default_caps_lock_off_fg")"
+    local off_bg_color="$(tmux-option "@caps_lock_off_bg" "$default_caps_lock_off_bg")"
 
-    local caps_lock_on_prefix="$(tmux-option "@caps_lock_on_prefix" "$default_caps_lock_on_prefix")"
-    local caps_lock_on_suffix="$(tmux-option "@caps_lock_on_suffix" "$default_caps_lock_on_suffix")"
-    local caps_lock_off_prefix="$(tmux-option "@caps_lock_off_prefix" "$default_caps_lock_off_prefix")"
-    local caps_lock_off_suffix="$(tmux-option "@caps_lock_off_suffix" "$default_caps_lock_off_suffix")"
+    local on_attr_opts="$(tmux-option "@caps_lock_on_attr" "$default_caps_lock_on_attr")"
+    local off_attr_opts="$(tmux-option "@caps_lock_off_attr" "$default_caps_lock_off_attr")"
+
+    on_fg="${on_fg_color:+#[fg=${on_fg_color}]}"
+    on_bg="${on_bg_color:+#[bg=${on_bg_color}]}"
+    off_fg="${off_fg_color:+#[fg=${off_fg_color}]}"
+    off_bg="${off_bg_color:+#[bg=${off_bg_color}]}"
+
+    on_attr="${on_attr_opts:+#[default]#[${on_attr_opts}]}"
+    off_attr="${off_attr_opts:+#[default]#[${off_attr_opts}]}"
 
     local status="$(xset -q | sed -rn 's/.*\s+Caps Lock:\s+(on|off)\s+.*/\1/p')"
 
     if [[ $status = "on" ]]; then
-        echo "#[fg=$caps_lock_on_fg]#[bg=$caps_lock_on_bg]${caps_lock_on_prefix}${caps_lock_on_value}${caps_lock_on_suffix}#[default]"
+        echo "${on_attr}${on_fg}${on_bg}${on_value}#[default]"
     else
-        echo "#[fg=$caps_lock_off_fg]#[bg=$caps_lock_off_bg]${caps_lock_off_prefix}${caps_lock_off_value}${caps_lock_off_suffix}#[default]"
+        echo "${off_attr}${off_fg}${off_bg}${off_value}#[default]"
     fi
 }
 
